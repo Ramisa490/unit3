3c600f48b2bf9824cf30dd6bc2ec59fea14de0f6
Ramisa <ramisaaust@gmail.com>
2023-12-24 13:45:28 +0600

    committed

--- src/test/java/framework/utils/StringUtils.java
+++ src/test/java/framework/utils/StringUtils.java
@@ -4,7 +4,7 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class StringUtils {
-    private final String DOUBLE_REGEX = "[^\\d.]";
+    private static final String DOUBLE_REGEX = "[^\\d.]";
 
     public static Double getDoubleFromString(String text) {
         return Double.parseDouble(text.replaceAll(DOUBLE_REGEX, ""));
