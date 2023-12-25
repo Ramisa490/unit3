c46d6c5fdf27012a1f3ff4629baff3b92d995e7b
Ramisa <ramisaaust@gmail.com>
2023-12-26 00:02:48 +0600

    committed

--- src/test/java/framework/tests/FileUploadTest.java
+++ src/test/java/framework/tests/FileUploadTest.java
@@ -1,6 +1,8 @@
 package framework.tests;
 
+import framework.pages.FileUploadPage;
 import framework.pages.MainPageNavigation;
+import framework.pages.UplodedFilePage;
 import framework.utils.SettingsTestData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,10 +13,15 @@ import java.io.File;
 public class FileUploadTest extends BaseTest {
     public final String FILE_NAME = SettingsTestData.getFileData().getUploadFile();
     public final String FILE_PATH = SettingsTestData.RESOURCE_FILE_PATH + FILE_NAME;
+    private FileUploadPage fileUploadPage = new FileUploadPage();
+
 
 
     @Test
     public void fileUploadTest() {
-        // todo: add test
+        mainPage.clickNavigationLink(MainPageNavigation.FILE_UPLOAD);
+        FileUploadPage.UploadFile(new File(FILE_PATH).getAbsolutePath());
+        FileUploadPage.clicksubmitbtn();
+        Assert.assertEquals(UplodedFilePage.getfilename(),FILE_NAME,"File name is not correct");
     }
 }
